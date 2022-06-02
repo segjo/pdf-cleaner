981aa788fe2c05e084c7e5b0290013ada4d69e95
Kevin Fechtel <kevin.fechtel@circle-web.de>
2022-06-02 13:44:32 +0200

    Implemented new Version of Process Document

--- plugins/http/src/main/java/com/dvelop/archetype/plugins/http/resources/ProcessDocument.java
+++ plugins/http/src/main/java/com/dvelop/archetype/plugins/http/resources/ProcessDocument.java
@@ -1,7 +1,6 @@
 package com.dvelop.archetype.plugins.http.resources;
 
 import org.apache.pdfbox.Loader;
-import org.apache.pdfbox.io.IOUtils;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.text.PDFTextStripper;
@@ -10,12 +9,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.FileOutputStream;
 import java.net.MalformedURLException;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -41,22 +35,15 @@ public class ProcessDocument {
     @GET
     @Produces({MediaType.APPLICATION_JSON, "application/hal+json"})
     public Response processDocument(@QueryParam("filePath") String filePath) throws MalformedURLException, IOException {        
-        boolean newVersion=false;
         //Aufrufen der anderen Funktion
         int pagecount = 0;
-        List<Integer> pagesToRemove = new ArrayList<>();
-
-        //PDDocument document = PDDocument.load(file);
-        byte[] buffer = java.nio.file.Files.readAllBytes(Paths.get(filePath));
-
-        File file = new File("src/test/resources/targetFile.tmp");
-        OutputStream outStream = new FileOutputStream(file);
-        outStream.write(buffer);
-
-        IOUtils.closeQuietly(outStream);
+        boolean newVersion = false;
+        File file = new File(filePath);
         PDDocument document = Loader.loadPDF(file);
+        PDDocument outDocument = new PDDocument();
+        
         if(document.getNumberOfPages()>1){
-            for (PDPage pdPage : document.getPages()) {
+            for (PDPage page : document.getPages()) {
                 PDFTextStripper textStripper = new PDFTextStripper();
                 textStripper.setStartPage(pagecount+1);
                 textStripper.setEndPage(pagecount+1);
@@ -64,19 +51,17 @@ public class ProcessDocument {
                 text = text.trim();
                 if (text == null || text.equals("")) {
                     newVersion=true;
-                    pagesToRemove.add(pagecount);
+                } else {
+                    outDocument.addPage(page);
                 }
                 pagecount++;
             }
+            
+            outDocument.save(file);
+            outDocument.close();
+            document.close();
+            
         }
-
-        for (Integer pagenumber : pagesToRemove) {
-            System.out.println("remove page: " + pagenumber);
-            document.removePage(pagenumber);
-        }
-
-        document.save(file);
-        document.close();
 
         if(newVersion){
             return Response.ok(file.getAbsolutePath()).build();
